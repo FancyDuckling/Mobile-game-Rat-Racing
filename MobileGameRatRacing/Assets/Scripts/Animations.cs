d0b8a545580dd4ac20a7301f2d1b915dc7f60c84
FancyDuckling <FancyDuckling@users.noreply.github.com>
2024-02-12 16:32:13 +0100

    test animation

---
+++ MobileGameRatRacing/Assets/Scripts/Animations.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Animations 
+{
+    public string name;
+    public AnimationClip clip;
+    
+}
--- MobileGameRatRacing/Assets/Scripts/PlayerController.cs
+++ MobileGameRatRacing/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class PlayerController : CarController
 {
     public float jumpForce;
-    public Animator playerAnimator;
+    public Animator animator;
+    private bool isJumping = false;
 
     protected override void Update()
     {
@@ -13,21 +14,56 @@ public class PlayerController : CarController
         
         if (controller.isGrounded && SwipeManager.swipeUp)
         {
+            
             Jump();
-           
+            
+
+
         }
+
+
     }
 
     private void Jump()
     {
+
+        // Check if not already jumping
+        if (!isJumping)
+        {
+            // Set jumping flag
+            isJumping = true;
+            // Set jump trigger
+            animator.SetBool("isJumping", true);
+            direction.y = jumpForce;
+            FindAnyObjectByType<AudioManager>().PlaySound("Jump");
+
+            // Reset the jumping flag after a short delay
+            StartCoroutine(ResetJumpFlag());
+        }
+
+
+        /* animator.SetTrigger("jumpingTrigger");
         direction.y = jumpForce;
-        FindAnyObjectByType<AudioManager>().PlaySound("Jump");
+        FindAnyObjectByType<AudioManager>().PlaySound("Jump");*/
+
+       
+
 
         /*if (playerAnimator != null)
         {
             playerAnimator.SetTrigger("jumpingTrigger"); // "JumpTrigger" is the name of the jump animation trigger parameter
         }*/
 
+    }
+
+    private IEnumerator ResetJumpFlag()
+    {
+        // Wait for a short delay before resetting the flag
+        yield return new WaitForSeconds(0.1f);
+        // Reset the jump flag
+        isJumping = false;
+        // Reset the jump trigger in the animator
+        animator.SetBool("isJumping", false);
     }
 
 }
