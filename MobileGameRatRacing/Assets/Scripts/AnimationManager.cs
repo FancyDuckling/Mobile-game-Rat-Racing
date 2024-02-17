cf928657369696938950af78e91308e01be6810e
FancyDuckling <FancyDuckling@users.noreply.github.com>
2024-02-17 02:17:26 +0100

    animations

--- MobileGameRatRacing/Assets/Scripts/AnimationManager.cs
+++ MobileGameRatRacing/Assets/Scripts/AnimationManager.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class AnimationManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Animator animator;
+    private string currentState;
+    const string IDLEANIM = "IdleAnim";
+    const string TILTLEFTANIM = "TiltLeftAnim";
+    const string TILTRIGHTANIM = "TiltRightAnim";
+    const string JUMPANIM = "JumpAnim";
+
+    private void Start()
     {
-        
+        animator = gameObject.GetComponent<Animator> ();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (SwipeManager.swipeLeft)
+            ChangeAnimationState(TILTLEFTANIM);
+
+        if (SwipeManager.swipeRight)
+            ChangeAnimationState(TILTRIGHTANIM);
+
+        if (SwipeManager.swipeUp)
+            ChangeAnimationState(JUMPANIM);
+
     }
+
+    private void ChangeAnimationState(string newState)
+    {
+
+        if (newState == currentState)
+            return;
+
+        animator.Play(newState);
+        currentState = newState;
+    }
+
+
+
+    //if swipe up ChangeAnimationState(JUMPANIM);
 }
